355a2503ee31026f6ec41c05b6768627f37e9f1c
M. Tayyab <tayyabnassem246@gmail.com>
2024-12-06 22:57:51 +0500

    Committing before resolving conflict

--- cafeFrontend/src/pages/DishDetails.js
+++ cafeFrontend/src/pages/DishDetails.js
@@ -26,20 +26,16 @@ function DishDetails() {
         Back to Menu
       </button>
       <div className={styles.dishDetailsCard}>
-        <img src={dish.image} alt={dish.name} className={styles.dishImage} />
+        <img src={dish.imgPath} alt={dish.name} className={styles.dishImage} />
         <h1 className={styles.dishName}>{dish.name}</h1>
         <p className={styles.dishCategory}>Category: {dish.category}</p>
         <p className={styles.dishprice}>Price {dish.price}</p>
         <p className={styles.dishDescription}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-          condimentum orci at felis ultrices, vel fringilla nulla lacinia.
-          Suspendisse potenti.
+          {dish.description}
         </p>
         <div className={styles.buttonsContainer}>
-        <button className={styles.crudButtonsButton}>Add a New Dish</button>
         <button className={styles.crudButtonsButton}>Update Dish</button>
         <button className={styles.crudButtonsButton}>Delete Dish</button>
-        <button className={styles.crudButtonsButton}>Mark as Special</button>
         <button className={styles.crudButtonsButton}>Mark as out of stcok</button>
         </div>
       </div>
